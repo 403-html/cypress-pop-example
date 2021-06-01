3d84de0f4001497017c7f6e84f0e3d33547fad92
htd-tstepien <tymoteusz.stepien@htdevelopers.com>
2021-06-01 10:49:17 +0200

    comma...

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -1,13 +1,13 @@
 const methodsPageLocators = {
   url: "https://example.cypress.io/",
   link: { tag: "a", locator: "get" },
-},
+};
 
 const queryingPageLocators = {
   url: "https://example.cypress.io/commands/querying",
   getHeader: { tag: "h4", locator: "cy.get()" },
   sampleButton: "#query-btn",
-}
+};
 
 export default {
   methodsPageLocators,
