eb26b6fe156ed89c68adcac0e886a610dfdaca66
htd-tstepien <tymoteusz.stepien@htdevelopers.com>
2021-06-01 10:48:17 +0200

    Moved locators to more readable form

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -1,11 +1,15 @@
+const methodsPageLocators = {
+  url: "https://example.cypress.io/",
+  link: { tag: "a", locator: "get" },
+},
+
+const queryingPageLocators = {
+  url: "https://example.cypress.io/commands/querying",
+  getHeader: { tag: "h4", locator: "cy.get()" },
+  sampleButton: "#query-btn",
+}
+
 export default {
-  methodsPageLocators: {
-    url: "https://example.cypress.io/",
-    link: { tag: "a", locator: "get" },
-  },
-  queryingPageLocators: {
-    url: "https://example.cypress.io/commands/querying",
-    getHeader: { tag: "h4", locator: "cy.get()" },
-    sampleButton: "#query-btn",
-  },
+  methodsPageLocators,
+  queryingPageLocators,
 };
